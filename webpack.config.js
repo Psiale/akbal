45092c11565cc8f33ae5cfd3b9dd789b3bdabf33
Alexis <psialedev@gmail.com>
2020-11-06 13:04:21 -0600

    Currently working on the hero section

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,6 @@ const path = require('path');
 // only uncomment if you run npm run apiKey
 // const Dotenv = require('dotenv-webpack');
 const PreloadWebpackPlugin = require('preload-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -11,7 +9,7 @@ module.exports = {
     app: './src/scripts/index.js',
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(__dirname, 'build'),
     compress: true,
     port: 9000,
   },
@@ -23,10 +21,6 @@ module.exports = {
   plugins: [
     // only uncomment if you run npm run apiKey
     // new Dotenv(),
-    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
-    new HTMLWebpackPlugin({
-      title: 'Weather App',
-    }),
     new PreloadWebpackPlugin(),
   ],
   module: {
